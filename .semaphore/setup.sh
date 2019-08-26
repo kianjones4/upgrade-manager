7f54e644d779b6c1c2f0247f280cdf908d540b26
kjones16 <kian_jones@intuit.com>
2019-08-26 16:25:16 -0700

    build

--- .semaphore/setup.sh
+++ .semaphore/setup.sh
@@ -3,13 +3,15 @@
 if [ ! -d '/packages' ]; then
   sudo mkdir -p /packages
   wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
-  sudo tar -xvf go1.12.7.linux-amd64.tar.gz
-  sudo mv go /packages
+  # sudo tar -xvf go1.12.7.linux-amd64.tar.gz
+  # sudo mv go /packages
+  sudo mv go1.12.7.linux-amd64.tar.gz /packages
   export os=$(go env GOOS)
   export arch=$(go env GOARCH)
-  curl -sL https://go.kubebuilder.io/dl/latest/${os}/${arch} | tar -xz -C /packages
+  curl -sL https://go.kubebuilder.io/dl/latest/${os}/${arch} -o kubebuilder_master_${os}_${arch}.tar.gz # | tar -xz -C /packages
+  sudo mv kubebuilder_master_${os}_${arch}.tar.gz /packages
   # sudo cp -r /tmp/kubebuilder_master_${os}_${arch} /packages/kubebuilder_master_${os}_${arch}
-  ls /packages
+  ls -l /packages
   cache store $SEMAPHORE_PROJECT_NAME-deps /packages
   # wget https://go.kubebuilder.io/dl/latest/${os}/${arch} -O /packages
   # sudo tar -xvf go1.12.7.linux-amd64.tar.gz -C /packages/go
