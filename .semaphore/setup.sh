d3c103cbc7bca00a3d89b52d3d538c377a31f5a0
kjones16 <kian_jones@intuit.com>
2019-08-26 15:23:09 -0700

    build

---
+++ .semaphore/setup.sh
@@ -0,0 +1,24 @@
+cache restore $SEMAPHORE_PROJECT_NAME-deps
+
+if [ ! -d '/packages']; then
+  sudo mkdir -p /packages/go
+  sudo mkdir -p /packages/kubebuilder
+  wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz -O /packages/
+  curl -sL https://go.kubebuilder.io/dl/latest/${os}/${arch} | tar -xz -C /packages/
+  cache store $SEMAPHORE_PROJECT_NAME-deps /packages
+  # wget https://go.kubebuilder.io/dl/latest/${os}/${arch} -O /packages
+  # sudo tar -xvf go1.12.7.linux-amd64.tar.gz -C /packages/go
+  # sudo cp go/bin/go /usr/local/bin
+  # sudo rm -rf /usr/local/go
+  # sudo mv go /usr/local
+  # # go version
+  # # which go
+  # # mkdir -p ~/go/bin
+  # export os=$(go env GOOS)
+  # export arch=$(go env GOARCH)
+  # # download the release
+  # curl -sL https://go.kubebuilder.io/dl/latest/${os}/${arch} | tar -xz -C /packages
+  # sudo cp -r /tmp/kubebuilder_master_${os}_${arch} /packages/kubebuilder
+  #
+  # export PATH=$PATH:/usr/local/kubebuilder/bin
+fi
